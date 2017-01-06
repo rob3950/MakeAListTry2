5df4c1e8fa0486e00584e136ba5ee2bb87e90df3
Rob_inHood <pokhrelrob@gmail.com>
2017-01-06 17:49:32 -0600

    Nothing New here

---
+++ MakeAListTry2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MakeAListTry2
+//
+//  Created by Mac User on 1/6/17.
+//  Copyright © 2017 UglyFish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
